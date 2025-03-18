bd43da7e18b5d879c034e409f708b36583c28bad
martinurd <martineurdapilleta@gmail.com>
2025-03-18 00:16:29 -0400

    Improved SEO metadata on page.jsx

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import Banner from '../../components/banner/Banner'
 
 export const metadata = {
-  title: 'Home',
-  description: 'Henry Capital provides flexible liquidity solutions for private equity investors and professionals. Access innovative financial solutions for your PE investments.',
-  keywords: ['private equity', 'liquidity solutions', 'PE investments', 'fund management', 'capital solutions', 'Henry Capital'],
+  title: 'Henry Capital | Liquidity Solutions for Private Equity Investors',
+  description: 'Flexible liquidity solutions for private equity investors and professionals. Access innovative financial solutions for your PE investments.',
+  keywords: ['private equity', 'leverage for private shares', 'GP commit loan', 'fund finance', 'GP commit financing', 'Henry Capital'],
   openGraph: {
     title: 'Henry Capital | Private Equity Liquidity Solutions',
-    description: 'Access innovative financial solutions for your PE investments',
+    description: 'Innovative loans against the value of your PE investments',
   }
 }
 
