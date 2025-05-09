1d68b6d89e945b5541a1d4dc16694d888292b85a
martinurd <martineurdapilleta@gmail.com>
2025-05-09 04:34:38 -0400

    Icons

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import RootLayout from '../../components/layout/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function SiteLayout({ children }) {
     return (
         <div>
             <RootLayout>
                 {children}
+                <Analytics />
             </RootLayout>
         </div>
-    )
+    );
 }
