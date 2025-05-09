6686ea269fe02717a21a74480700b6f01e0f2629
martinurd <martineurdapilleta@gmail.com>
2025-05-09 06:21:30 -0400

    Fin

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import RootLayout from '../../components/layout/Layout';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 export default function SiteLayout({ children }) {
     return (
@@ -8,6 +10,7 @@ export default function SiteLayout({ children }) {
             <RootLayout>
                 {children}
                 <Analytics />
+                <SpeedInsights />
             </RootLayout>
         </div>
     );
